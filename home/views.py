0fca5c00d88f931866ddb22412fae67cf1b33b44
sgarg26 <siddharth261202@gmail.com>
2024-02-22 15:12:22 -0500

    new post button added

--- home/views.py
+++ home/views.py
@@ -1,5 +1,22 @@
+from django import forms
 from django.shortcuts import render
+
+
+class NewTaskForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content")
+
 
 # Create your views here.
 def index(request):
     return render(request, "home/index.html")
+
+
+def new(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            new_post = {"title": title, "content": content}
+
