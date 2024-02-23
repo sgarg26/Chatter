eeea956563edac7fc72da3c3707cf039ef5b7858
Siddharth <siddharth261202@gmail.com>
2024-02-22 22:23:45 -0500

    Infinite scroll test

---
+++ home/static/home/index.js
@@ -0,0 +1,7 @@
+window.onscroll = () => {
+    if (window.innerHeight + window.scrollY >= (0.9 * document.body.offsetHeight)) {
+        const post = document.getElementsByClass("posts")[0]
+        post.
+
+    }
+}
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+posts = []
 
 class NewTaskForm(forms.Form):
     link = forms.CharField(label="Link to image")
@@ -12,14 +13,13 @@ class NewTaskForm(forms.Form):
 def index(request):
     return render(request, "home/index.html")
 
-
 def new(request):
+    new_post = {}
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
             link = form.cleaned_data["link"]
             desc = form.cleaned_data["desc"]
-            print(f"{link=}, {desc=}")
     return render(request, "home/new.html", {
         "form": NewTaskForm()
     })
