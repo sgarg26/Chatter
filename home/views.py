8bfac44f67db9a43b27dafb6db94e0012baecb6a
sgarg26 <siddharth261202@gmail.com>
2024-02-27 04:07:00 -0500

    feat: user imgs added to posts structure

--- home/views.py
+++ home/views.py
@@ -1,26 +1,29 @@
+from django import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django import forms
 from django.urls import reverse
 
 # Create your views here.
 posts = [
     {
         "link": "https://images.pexels.com/photos/20413769/pexels-photo-20413769/free-photo-of-a-black-and-white-photo-of-a-swan-in-the-water.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "desc": "Picture of swan"
+        "desc": "Picture of swan",
     },
     {
-        "link":  "https://images.pexels.com/photos/20416961/pexels-photo-20416961/free-photo-of-street-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "desc": "Picture of restaurant"
-    }
+        "link": "https://images.pexels.com/photos/20416961/pexels-photo-20416961/free-photo-of-street-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "desc": "Picture of restaurant",
+    },
 ]
+
 
 class NewForm(forms.Form):
     link = forms.CharField(label="Link to image")
     desc = forms.CharField(label="Description")
 
+
 def index(request):
     return render(request, "home/index.html")
+
 
 def create(request):
     if request.method == "POST":
@@ -28,11 +31,10 @@ def create(request):
         if form.is_valid():
             link = form.cleaned_data["link"]
             desc = form.cleaned_data["desc"]
-            new_post = {
-                "link": link,
-                "desc": desc
-            }
+            new_post = {"link": link, "desc": desc}
             posts.insert(0, new_post)
+            print("donezo")
         return HttpResponseRedirect(reverse("home:create"))
-    return render(request, "home/create.html", {"form":NewForm()})
+    return render(request, "home/create.html", {"form": NewForm()})
     # return render(request, "home/create.html")
+
