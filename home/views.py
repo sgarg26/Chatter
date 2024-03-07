9a6337e22de083d3d655fd5898c670dc57c3486a
sgarg26 <siddharth261202@gmail.com>
2024-03-07 10:45:42 -0500

    post ids

--- home/views.py
+++ home/views.py
@@ -3,24 +3,28 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+curr_id = 2
+
 # Create your views here.
 posts = [
     {
         "link": "https://images.pexels.com/photos/20413769/pexels-photo-20413769/free-photo-of-a-black-and-white-photo-of-a-swan-in-the-water.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "desc": "Picture of swan",
         "uname": "u1",
-        "num_likes": 12,
-        "comments": [
-            {
-                "commentUserName": "u2",
-                "comment": "Nice post",
-            },
-        ],
-        "post_id": 1,
+        # "num_likes": 12,
+        # "comments": [
+        #     {
+        #         "commentUserName": "u2",
+        #         "comment": "Nice post",
+        #     },
+        # ],
+        "post_id": 2,
     },
     {
         "link": "https://images.pexels.com/photos/20416961/pexels-photo-20416961/free-photo-of-street-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "desc": "Picture of restaurant",
+        "uname": "Person2",
+        "post_id": 1,
     },
 ]
 
@@ -52,11 +56,13 @@ def create(request):
             desc = form.cleaned_data["desc"]
             uname = form.cleaned_data["uname"]
             profile_pic = form.cleaned_data["profile_pic"]
+            curr_id += 1
             new_post = {
                 "link": link,
                 "desc": desc,
                 "uname": uname,
                 "profile_pic": profile_pic,
+                "post_id": curr_id
             }
             posts.insert(0, new_post)
         return HttpResponseRedirect(reverse("home:index"))
